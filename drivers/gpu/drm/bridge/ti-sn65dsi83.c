f34f5e467a04bbea9d084fb96a9268bef918fe4b
spokV <assaf1111@yahoo.com>
2022-08-03 21:54:54 +0300

    stam

--- drivers/gpu/drm/bridge/ti-sn65dsi83.c
+++ drivers/gpu/drm/bridge/ti-sn65dsi83.c
@@ -700,6 +700,8 @@ static int sn65dsi83_probe(struct i2c_client *client,
 	struct sn65dsi83 *ctx;
 	int ret;
 
+	dev_err(dev, "sn65dsi83_probe\n");
+	
 	ctx = devm_kzalloc(dev, sizeof(*ctx), GFP_KERNEL);
 	if (!ctx)
 		return -ENOMEM;
