9d6d9a9eb1bac43e67f0cf183e07309f2b75d248
spokV <assaf1111@yahoo.com>
2022-08-30 13:46:15 +0300

    add scripts

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+KERNEL=kernel8
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+
+#edit .config
+#CONFIG_DRM_TI_SN65DSI83=m
+sed -i '/CONFIG_DRM_TI_SN65DSI83/c\CONFIG_DRM_TI_SN65DSI83=m' .config
+
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+
+#mount cm4 to pc (to /media/spok/boot and /media/spok/rootfs as example)
+
+#sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=/media/spok/rootfs modules_install
+
+#sudo cp /media/spok/boot/$KERNEL.img /media/spok/boot/$KERNEL-backup.img
+#sudo cp arch/arm64/boot/Image /media/spok/boot/$KERNEL.img
+#sudo cp arch/arm64/boot/dts/broadcom/*.dtb /media/spok/boot/
+#sudo cp arch/arm64/boot/dts/overlays/*.dtb* /media/spok/boot/overlays/
+#sudo cp arch/arm64/boot/dts/overlays/README /media/spok/boot/overlays/
+#sudo umount /media/spok/boot
+#sudo umount /media/spok/rootfs
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+KERNEL=kernel8
+#make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+
+#edit .config
+#CONFIG_DRM_TI_SN65DSI83=m
+
+#make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+
+#mount cm4 to pc (to /media/spok/boot and /media/spok/rootfs as example)
+
+sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=/media/spok/rootfs modules_install
+
+echo "copying files...."
+sudo cp /media/spok/boot/$KERNEL.img /media/spok/boot/$KERNEL-backup.img
+sudo cp arch/arm64/boot/Image /media/spok/boot/$KERNEL.img
+sudo cp arch/arm64/boot/dts/broadcom/*.dtb /media/spok/boot/
+sudo cp arch/arm64/boot/dts/overlays/*.dtb* /media/spok/boot/overlays/
+sudo cp arch/arm64/boot/dts/overlays/README /media/spok/boot/overlays/
+
+echo "copy config file"
+sudo cp /media/spok/boot/config.txt /media/spok/boot/config-backup.txt
+sudo cp config.txt /media/spok/boot/
+echo "done!"
+#sudo umount /media/spok/boot
+#sudo umount /media/spok/rootfs
