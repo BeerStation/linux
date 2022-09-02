f45dc1e446aaf28c90d6ac088c271f83a5826660
spokV <assaf1111@yahoo.com>
2022-09-02 12:06:53 +0300

    add home_PATH var to install scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+HOME_PATH=/media/spok
 KERNEL=kernel8
 #make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
 
@@ -10,18 +11,18 @@ KERNEL=kernel8
 
 #mount cm4 to pc (to /media/spok/boot and /media/spok/rootfs as example)
 
-sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=/media/spok/rootfs modules_install
+sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=$HOME_PATH/rootfs modules_install
 
 echo "copying files...."
-sudo cp /media/spok/boot/$KERNEL.img /media/spok/boot/$KERNEL-backup.img
-sudo cp arch/arm64/boot/Image /media/spok/boot/$KERNEL.img
-sudo cp arch/arm64/boot/dts/broadcom/*.dtb /media/spok/boot/
-sudo cp arch/arm64/boot/dts/overlays/*.dtb* /media/spok/boot/overlays/
-sudo cp arch/arm64/boot/dts/overlays/README /media/spok/boot/overlays/
+sudo cp $HOME_PATH/boot/$KERNEL.img $HOME_PATH/boot/$KERNEL-backup.img
+sudo cp arch/arm64/boot/Image $HOME_PATH/boot/$KERNEL.img
+sudo cp arch/arm64/boot/dts/broadcom/*.dtb $HOME_PATH/boot/
+sudo cp arch/arm64/boot/dts/overlays/*.dtb* $HOME_PATH/boot/overlays/
+sudo cp arch/arm64/boot/dts/overlays/README $HOME_PATH/boot/overlays/
 
 echo "copy config file"
-sudo cp /media/spok/boot/config.txt /media/spok/boot/config-backup.txt
-sudo cp config.txt /media/spok/boot/
+sudo cp $HOME_PATH/boot/config.txt $HOME_PATH/boot/config-backup.txt
+sudo cp config.txt $HOME_PATH/boot/
 echo "done!"
-#sudo umount /media/spok/boot
-#sudo umount /media/spok/rootfs
+#sudo umount $HOME_PATH/boot
+#sudo umount $HOME_PATH/rootfs
